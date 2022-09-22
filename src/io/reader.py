9ded125d098f45b7867ae1f0f8488cbf86ed55fb
vdella <vitor.origamer@gmail.com>
2022-09-22 20:13:35 -0300

    added docs

--- src/io/reader.py
+++ src/io/reader.py
@@ -1,7 +1,8 @@
 from __init__ import resource_dir
 
 
-def read(file):
+def read(file) -> str:
+    """Reads a file and :returns all of its content as a single string."""
     with open(resource_dir / file) as f:
         lines = f.read()
 
