8d4e6349c2fec9fbcf365a38f3280770b2614abc
vdella <vitor.origamer@gmail.com>
2022-09-23 11:34:35 -0300

    changed prettytable by tabulate

--- src/symbol_table.py
+++ src/symbol_table.py
@@ -1,4 +1,4 @@
-from prettytable import PrettyTable
+from tabulate import tabulate
 
 
 class SymbolTable:
@@ -17,16 +17,8 @@ class SymbolTable:
                     self.content[token[3]] = (token[0], token[1], [])
 
     def __str__(self):
-        printer = PrettyTable()
-        printer.field_names = ["Identifier", "Position", "Declaration line", "Reference line"]
-
-        for line in self.content.items():
-            identifier = line[0]
-            position, declaration_line, reference_line = line[1]
-
-            printer.add_row([identifier, position, declaration_line, reference_line])
-
-        return str(printer)
+        headers = ["Identifier", "Position", "Declaration line", "Reference line"]
+        return tabulate([(k,) + v for k, v in self.content.items()], headers=headers, tablefmt='fancy_grid')
 
     @staticmethod
     def token_list_for(given_lexer) -> list:
