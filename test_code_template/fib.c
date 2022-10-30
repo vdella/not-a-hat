b4ca6aba037cd8e2f2778638bc820a888534b279
vdella <vitor.origamer@gmail.com>
2022-10-30 20:02:28 -0300

    changed test code to return function calls

--- test_code_template/fib.c
+++ test_code_template/fib.c
@@ -1,7 +1,26 @@
 def fib(int n)
 {
-    if (n == 1) {
+    if (n == 1)
+    {
         return 1;
     }
-    return fib(n - 1) + fib(n - 2)
+
+    if (n == 2)
+    {
+        return 1;
+    }
+
+    int first = n - 1;
+    int second = n - 2;
+
+    int fib_first = fib(first);
+    int fib_second = fib(second);
+    int result = fib_first + fib_second;
+
+    return result;
+}
+
+def main()
+{
+    return fib(3);
 }
